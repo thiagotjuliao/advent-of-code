4ec802f9837d264fc8529a4701a5c095619e1686
Thiago <ttj.math@gmail.com>
2026-09-20 21:35:47 -0300

    2015 day 01
    Both parts verified with: sbt "run check 2015 1"

--- src/main/scala/aoc/y2015/Day01.scala
+++ src/main/scala/aoc/y2015/Day01.scala
@@ -7,4 +7,24 @@ import aoc.Solution
   * To solve it, register the parts:
   *   part1 { in => in.lines.size }
   */
-object Day01 extends Solution(2015, 1)
+object Day01 extends Solution(2015, 1):
+  private def parse(c: Char): Int =
+    c match
+      case '(' => 1
+      case ')' => -1
+
+  part1 {
+    _.text.map(parse).sum
+  }
+
+  part2 {
+    @scala.annotation.tailrec
+    def solve(s: String, n: Int = 0, f: Int = 0): Int =
+      if n == s.size then -1
+      else
+        val f_ = f + parse(s(n))
+        if f_ == -1 then n + 1
+        else solve(s, n + 1, f_)
+
+    input => solve(input.text)
+  }
---
+++ src/test/scala/aoc/y2015/Day01Suite.scala
@@ -0,0 +1,23 @@
+package aoc.y2015
+
+import aoc.AOCFunSuite
+import aoc.y2015.Day01
+import aoc.Input
+
+class Day01Suite extends AOCFunSuite(Day01):
+  part1 {
+    sample("sample00", Input("(())"), 0)
+    sample("sample01", Input("()()"), 0)
+    sample("sample02", Input("(()(()("), 3)
+    sample("sample03", Input("))((((("), 3)
+    sample("sample04", Input("((("), 3)
+    sample("sample05", Input("())"), -1)
+    sample("sample06", Input("))("), -1)
+    sample("sample07", Input(")))"), -3)
+    sample("sample08", Input(")())())"), -3)
+  }
+
+  part2 {
+    sample("sample00", Input(")"), 1)
+    sample("sample01", Input("()())"), 5)
+  }
